0b9856c984bfa1d7af20216678c4241500a1a0cc
Alessia Amore <a.amore9393@gmail.com>
2022-03-09 15:33:00 +0100

    header fixed burger menu homepage

---
+++ main.js
@@ -0,0 +1,10 @@
+const showNav = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".menu");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("menu-active");
+  });
+};
+
+showNav();
