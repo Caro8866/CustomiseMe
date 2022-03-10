d54c5a7579ef6c245be2f3e9b0581856f8ee7563
Caroline Thostrup <caro342h@stud.kea.dk>
2022-03-10 18:51:16 +0100

    added spinny text javascript to index.js

--- index.js
+++ index.js
@@ -56,3 +56,11 @@ const showNav = () => {
 };
 
 showNav();
+
+const text = document.querySelector("#circleText a");
+text.innerHTML = text.innerText
+  .split("")
+  .map(
+    (char, i) => `<span style="transform:rotate(${i * 9.5}deg)">${char}</span>`
+  )
+  .join("");
